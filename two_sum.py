a331ea1e30982d6bd48d5b04bd9a618104bc3d42
adam <adam.lee346@gmail.com>
2021-10-07 14:32:19 -0600

    added twosum v2

--- two_sum.py
+++ two_sum.py
@@ -11,7 +11,23 @@ class TwoSum:
             else: 
                 req[nums[i]] = i
 
+    def two_sum_v2(self, nums, target):
+        print("The target is: ", target)
+        print("The list is: ", nums)
+        
+        cache = {}
+
+        for i in range(len(nums)):
+            index_of_num = nums[i]
+
+            if nums[i] in cache: 
+                return [cache[nums[i]], i]
+            cache[target - index_of_num] = i
+
+
+
 input_nums_list = [2, 4, 6, 8]
 ts = TwoSum()
 
 print("result: ", ts.two_sum(input_nums_list, 12))
+print("result (v2): ", ts.two_sum_v2(input_nums_list, 12))
