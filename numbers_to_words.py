07c0bc4acd7d7b593209816a464dcc09807e61ee
adam <adam.lee346@gmail.com>
2021-10-07 13:14:48 -0600

    number to words

---
+++ numbers_to_words.py
@@ -0,0 +1,24 @@
+'''
+convert a given number to a word
+ex. 1 = "one", 2 = "two", etc..
+'''
+
+class NumToWord: 
+    def num_to_word(self, num):
+        num_to_words = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 
+             6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', 
+            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 
+            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 
+            19: 'Nineteen', 20: 'Twenty', 30: 'Thirty', 40: 'Forty', 
+            50: 'Fifty', 60: 'Sixty', 70: 'Seventy', 80: 'Eighty', 
+            90: 'Ninety', 0: 'Zero'}
+
+        try:
+            print(num_to_words[num])
+        except KeyError:
+            print("Number is out of range")
+
+
+n2w = NumToWord()
+n2w.num_to_word(2)
+n2w.num_to_word(50)
