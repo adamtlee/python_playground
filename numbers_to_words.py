c2ec5fecf7b2838254dee911f3b2626a56c4942f
adam <adam.lee346@gmail.com>
2021-10-08 12:44:19 -0600

    num to word v2

--- numbers_to_words.py
+++ numbers_to_words.py
@@ -17,8 +17,32 @@ class NumToWord:
             print(num_to_words[num])
         except KeyError:
             print("Number is out of range")
-            
+
+    def num_to_word_v2(self, num):
+        less_than_20 = "One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen".split()
+        less_than_100 = "Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety".split()
+
+        def words(num):
+            if num == 0:
+                return []
+            elif num < 20:
+                return [ less_than_20[num-1] ]
+            elif num < 100:
+                return [ less_than_100[(num//10)-2] ] + words(num%10)
+            elif num < 1000:
+                return [ less_than_20[(num//100)-1] ] + ["Hundred"] + words(num%100)
+            else:
+                for i, p in enumerate("Thousand Million Billion".split(), 1):
+                    if num < 1000 ** (i+1):
+                        return words(num//1000**i) + [p] + words(num%1000**i)
+
+        print(" ".join(words(num)) or "Zero")
+        return " ".join(words(num)) or "Zero"
+
+
 
 n2w = NumToWord()
-n2w.num_to_word(2)
-n2w.num_to_word(50)
+n2w.num_to_word_v1(2)
+n2w.num_to_word_v1(50)
+
+n2w.num_to_word_v2(167)
